e81fc41950f837975b05f00770c93b1ab43af2dd
Daniel Gomez Rico <danielgomezrico@gmail.com>
2015-06-17 10:34:31 -0500

    :neckbeard: + Bob scala problem

--- scala/bob/src/main/scala/bob.scala
+++ scala/bob/src/main/scala/bob.scala
@@ -1,15 +1,24 @@
+
 class Bob {
 
-  def hey(text: String) : String = {
-  	if (text.endsWith("!") && text.toLowerCase != text){
-		"Whoa, chill out!"
-  	} else if (text.endsWith(".")){
-    	"Whatever."
-  	} else if (text.endsWith("?")){
-  		"Sure."
-  	} else {
-  		"Whatever."
-  	}
+  def isScreaming(text: String): Boolean = {
+    if (text.split(",").last.trim.forall(x => x.isDigit || !x.isLetter )){
+      false
+    } else {
+      text.toUpperCase() == text;
+    }
+  }
+
+  def hey(text: String): String = {
+    if (text.trim.isEmpty){
+      "Fine. Be that way!"
+    } else if (isScreaming(text)) {
+      "Whoa, chill out!"
+    } else if (text.endsWith("?")) {
+      "Sure."
+    } else {
+      "Whatever."
+    }
   }
 
 }
